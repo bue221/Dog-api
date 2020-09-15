f57ef13d61d85e30e6e9469aaabed04de1726e99
bue221 <camiloplaza3@gmail.com>
2020-09-14 20:34:18 -0500

    app de perros terminada

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import { TextField, Button } from '@material-ui/core/';
+
+const App = ()=>{
+
+        const [ query,setQuery ] = useState('affenpinscher')
+        const [ search, setSearch ] = useState('');
+        const [ data, setData ] = useState('');
+        //console.log(search);
+
+
+        useEffect(()=>{
+                getDog();
+        },[query]);
+
+        const getDog = async() =>{
+                let url = `https://dog.ceo/api/breed/${query}/images/random`;
+                const res = await fetch(url);
+                const data = await res.json();
+                setData(data);
+        }
+
+        const getRamdom = async() =>{
+                let url = "https://dog.ceo/api/breeds/image/random";
+                const res = await fetch(url);
+                const data = await res.json();
+                setData(data);
+        }
+
+        const boton = (e)=>{
+                e.preventDefault();
+                setQuery(search);
+        }
+
+        //console.log(data);
+
+
+        return(
+                <div className='contenedor'>
+                        <div className='busqueda'>
+                                <TextField id="busqueda" onChange={(e)=>setSearch(e.target.value)} label="Busqueda"/>
+                                <Button variant="contained" color="secondary" onClick={boton}>
+                                        Buscar
+                                </Button>
+                        </div>
+                        <Button variant="contained" color="primary" onClick={getRamdom}>
+                                Random
+                        </Button>
+                        <div className='img'>
+                                {  data.status == 'success' ? <img src={data.message} alt="img" />:
+                                <h1>Busqueda no encontrada</h1>
+                                }
+                        </div>
+                </div>
+                );
 }
 
 export default App;
