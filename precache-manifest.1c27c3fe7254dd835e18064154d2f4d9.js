aeb149906b5ec9490d1e1d34959335cc93134232
bue221 <camiloplaza3@gmail.com>
2020-09-21 04:45:51 -0500

    Updates

---
+++ precache-manifest.1c27c3fe7254dd835e18064154d2f4d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373b0d0a3e97c06c5d032886b490b703",
+    "url": "/Dog-api/index.html"
+  },
+  {
+    "revision": "03b2b35e4af2fe5147ce",
+    "url": "/Dog-api/static/css/main.dcd4bfcd.chunk.css"
+  },
+  {
+    "revision": "331b775cac2582cf5bd8",
+    "url": "/Dog-api/static/js/2.607b4684.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Dog-api/static/js/2.607b4684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b2b35e4af2fe5147ce",
+    "url": "/Dog-api/static/js/main.dd7d193b.chunk.js"
+  },
+  {
+    "revision": "deec289389ce0d316599",
+    "url": "/Dog-api/static/js/runtime-main.ca2bdd0c.js"
+  }
+]);
